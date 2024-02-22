9821ea4f7c63f18e71c59a8ff3e89ad63a9d83f7
Arnaz De Jesus <apdejesus@hau.edu.ph>
2024-02-22 22:34:59 +0800

    Init Commit Q4

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Container from "@mui/material/Container";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ProfileScreen from "./screens/ProfileScreen";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} exact/>
+              <Route path='login/' element={<LoginScreen />} />
+              <Route path='register/' element={<RegisterScreen />} />
+              <Route path='profile/' element={<ProfileScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
